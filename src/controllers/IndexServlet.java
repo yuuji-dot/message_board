07fdf8b409a2af032661694a843c193bf62dc961
kosar <kosar@LAPTOP-140PAQBL>
2022-08-29 03:51:22 +0900

    getAllMessagesを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,33 +1,33 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class IndexServlet
- */
+import models.Message;
+import util.DBUtil;
+
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private static final long serialVersionUID = 1L;
+
     public IndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+
+        em.close();
+    }
 
 }
