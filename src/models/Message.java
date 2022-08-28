ab9b474fe21dc1abb5b54ed7daf9aa74ed3d6663
kosar <kosar@LAPTOP-140PAQBL>
2022-08-28 18:05:26 +0900

    データベース関連のファイルを追加

---
+++ src/models/Message.java
@@ -0,0 +1,71 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Column(name ="content", length =255, nullable = false)
+    private String content;
+
+    @Column(name ="created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name ="update_at", nullable = false)
+    private Timestamp updates_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdates_at() {
+        return updates_at;
+    }
+
+    public void setUpdates_at(Timestamp updates_at) {
+        this.updates_at = updates_at;
+    }
+}
