5bc9cd03677e0bd4321a054c851846be8faa667d
kosar <kosar@LAPTOP-140PAQBL>
2022-09-01 00:50:28 +0900

    バリデーションを追加

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
 import util.DBUtil;
+import validators.MessageValidator;
 
 /**
  * Servlet implementation class CreateServlet
@@ -48,6 +51,21 @@ public class CreateServlet extends HttpServlet {
             m.setCreated_at(currentTime);
             m.setUpdated_at(currentTime);
 
+            //バリデーションを実行してエラーがあったら新規登録のフォームに戻る
+            List<String> errors =MessageValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
+
+                //フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("message", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+                rd.forward(request,  response);
+            }else {
+
+
           //データベースに保存
             em.persist(m);
             em.getTransaction().commit();
@@ -55,6 +73,7 @@ public class CreateServlet extends HttpServlet {
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
 }
--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
 import util.DBUtil;
+import validators.MessageValidator;
 
 /**
  * Servlet implementation class UpdateServlet
@@ -49,17 +52,32 @@ public class UpdateServlet extends HttpServlet {
             Timestamp currentTime =new Timestamp(System.currentTimeMillis());
             m.setUpdated_at(currentTime);
 
-            //データベースを更新
-            em.getTransaction().begin();
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "更新が完了しました。");
-            em.close();
+            //バリデーションを実行してエラーがあったら編集画面のフォームに戻る
+            List<String> errors = MessageValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
 
-            //セッションスコープ上の不要になったデータを削除
-            request.getSession().removeAttribute("message_id");
 
-            //indexページへリダイレクト
-            response.sendRedirect(request.getContextPath() + "/index");
+                //フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("message", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+                rd.forward(request, response);
+            }else {
+                //データベースを更新
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+                em.close();
+
+                //セッションスコープ上の不要になったデータを削除
+                request.getSession().removeAttribute("message_id");
+
+                //indexページへリダイレクト
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
 
